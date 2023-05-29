4ff1ee7ef90e07dfe96c6ebc1045bfb775caaa49
SomeBoringNerd <someboringnerd@gmail.com>
2023-05-29 13:39:31 +0200

    Added automatic git hash in watermark

--- src/main/java/lol/waifuware/Modules/GUI/Watermark.java
+++ src/main/java/lol/waifuware/Modules/GUI/Watermark.java
@@ -9,6 +9,10 @@ import lol.waifuware.Settings.BooleanSetting;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.client.MinecraftClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @Module(name = "Watermark", key = 0, cat = CATEGORY.GUI)
 public class Watermark extends AbstractModule
 {
@@ -16,6 +20,8 @@ public class Watermark extends AbstractModule
     public BooleanSetting ShowPronoun = new BooleanSetting("ShowPronoun", false, "Show your pronouns fetched from PronounDB", "sp");
     public BooleanSetting ShowVersion = new BooleanSetting("ShowVersion", true, "Show the version of WaifuHax you are using", "sv");
     public BooleanSetting ShowUsername = new BooleanSetting("ShowUsername", true, "Show your username", "su");
+
+    String latestHash = "";
 
     public Watermark()
     {
@@ -26,6 +32,8 @@ public class Watermark extends AbstractModule
         desc[0] = "Fancy text on your screen";
 
         offset = isEnabled() ? 10 : 0;
+
+        latestHash = executeGitCommand();
     }
     private static int offset;
     public static int getWatermarkOffset()
@@ -52,7 +60,30 @@ public class Watermark extends AbstractModule
     }                                                                                                           //
     //                                                                                                          //
     // -------------------------------------------------------------------------------------------------------- //
-    
+
+
+    public static String executeGitCommand()
+    {
+        StringBuilder output = new StringBuilder();
+
+        try {
+            Process process = Runtime.getRuntime().exec("git ls-remote -h git@github.com:SomeBoringNerd/WaifuHax.git");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                output.append(line);
+            }
+
+            reader.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return output.substring(0, 7);
+    }
 
     boolean marked;
     @EventHandler
@@ -60,6 +91,6 @@ public class Watermark extends AbstractModule
     {
         String name = MinecraftClient.getInstance().player.getEntityName();
 
-        MinecraftClient.getInstance().textRenderer.drawWithShadow(event.getMatrices(), "§c[§dWelcome to §5WaifuHax" + (ShowVersion.getEnabled() ?" V2" : "") + "§d" + (ShowUsername.getEnabled() ? (", " + name) : "") + "§4 ♥§c] " + (ShowPronoun.getEnabled() ? (Pronoun.self_pronoun != null ? "§4Pronouns : " + Pronoun.self_pronoun : "") + "§r" : ""), 5, 5, 0xFFFFFF);
+        MinecraftClient.getInstance().textRenderer.drawWithShadow(event.getMatrices(), "§c[§5WaifuHax git-" + latestHash + (ShowVersion.getEnabled() ?" V2" : "") + "§d" + (ShowUsername.getEnabled() ? (", " + name) : "") + "§4 ♥§c] " + (ShowPronoun.getEnabled() ? (Pronoun.self_pronoun != null ? "§4Pronouns : " + Pronoun.self_pronoun : "") + "§r" : ""), 5, 5, 0xFFFFFF);
     }
 }
